ec191647a3e442be83dcf5e9633bfaa5f5994c51
unknown <joelma.takeuchi@gmail.com>
2020-08-25 11:54:57 -0300

    implementação da página feed

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FeedComponent } from './feed/feed.component';
+import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    FeedComponent,
+    PerfilLateralComponent
   ],
   imports: [
     BrowserModule,
